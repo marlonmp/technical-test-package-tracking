e73c9aca19accc891053cfaeec7a658ce057da5a
marlonmp <76027579+marlonmp@users.noreply.github.com>
2024-11-26 02:49:26 -0500

    feat: data objects added

---
+++ DataObjects/DeliveryRequestDataObject.cs
@@ -0,0 +1,30 @@
+﻿using ccs.Models;
+
+namespace ccs.DataObjects
+{
+    public class DeliveryRequestListDTO
+    {
+        public int Id { get; set; }
+
+        public virtual AppUser User { get; set; } = null!;
+
+        public string Address { get; set; }
+
+        public DeliveryRequestStatus Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DeliveryRequestListDTO(DeliveryRequest deliveryRequest)
+        {
+            this.Id = deliveryRequest.Id;
+            this.User = deliveryRequest.User;
+            this.Address = deliveryRequest.Address;
+            this.Status = deliveryRequest.Status;
+
+            this.CreatedAt = deliveryRequest.CreatedAt;
+            this.UpdatedAt = deliveryRequest.UpdatedAt;
+        }
+    }
+}
---
+++ DataObjects/ProductDataObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ccs.Models;
+
+namespace ccs.DataObjects
+{
+    public class ProductCreateUpdateDTO
+    {
+        [Required]
+        [StringLength(48, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public Product ToModel(Product? product = default)
+        {
+            if (product != null)
+            {
+                product.Name = this.Name;
+                return product;
+            }
+
+            return new Product { Name = this.Name };
+        }
+    }
+}
